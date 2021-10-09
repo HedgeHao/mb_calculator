198f5d75be9b223d98be9e94c4a7e1c4922b9cd6
hedgehao <hedgehao@gmail.com>
2021-10-09 09:17:04 +0800

    .

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,8 +5,8 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "main.dart.js": "8db3cf461eb97858b13ac3d6f14816aa",
 "manifest.json": "7326f8da0c0463ce037c0f4ec0347eeb",
-"index.html": "6a3a20565a9559a226d72b6e202f4d02",
-"/": "6a3a20565a9559a226d72b6e202f4d02",
+"index.html": "c28d5a0f72a5514c0495848151d898e2",
+"/": "c28d5a0f72a5514c0495848151d898e2",
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
